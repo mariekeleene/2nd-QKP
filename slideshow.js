fa1a15fa639918188f5339daa7395fec5737b2af
m <mariekeleene@protonmail.com>
2026-03-09 10:56:06 +0000

    ok lets go 6

--- slideshow.js
+++ slideshow.js
@@ -42,4 +42,23 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    const button = document.getElementById("floating-back");
+
+    function moveButton() {
+
+        const w = window.innerWidth - button.offsetWidth;
+        const h = window.innerHeight - button.offsetHeight;
+
+        const x = Math.random() * w;
+        const y = Math.random() * h;
+
+        button.style.left = x + "px";
+        button.style.top = y + "px";
+    }
+
+    /* move every 2 seconds */
+
+    moveButton();
+    setInterval(moveButton, 2000);
+
 });
